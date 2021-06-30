f30433b8863baef8cb31494d1a4ac42794ce58cf
Bosnea-Samira <samirabosnea@gmail.com>
2021-06-30 12:36:19 +0300

    adding changes

---
+++ todolist.js
@@ -0,0 +1,16 @@
+
+
+
+
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector("#newtask input").value.length == 0) {
+        alert("Please enter a task")
+    } else {
+    document.querySelector("#tasks").innerHTML += 
+    `<div class="task">
+    <span id="taskname">
+    ${document.querySelector("#newtask input").value}
+    </span>
+    </div>`;
+    }
+}
