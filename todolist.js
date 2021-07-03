dbb847ab78f8971983d778491b531b057fe74f17
Bosnea-Samira <samirabosnea@gmail.com>
2021-07-03 18:27:41 +0300

    adding changes

--- todolist.js
+++ todolist.js
@@ -11,6 +11,26 @@ document.querySelector('#push').onclick = function(){
     <span id="taskname">
     ${document.querySelector("#newtask input").value}
     </span>
+    <button class="delete">
+    <i class = "fa fa-trash">
+    </i>
+    
+    </button>
     </div>`;
+
+    let currentTasks = document.querySelectorAll(".delete");
+    for(let i = 0; i < currentTasks.length; i++) {
+        currentTasks[i].onclick = function() {
+            this.parentNode.remove();
+        }
     }
+    let tasks = document.querySelectorAll(".tasks");
+    for(let i = 0; i<tasks.length; i++) {
+        tasks[i].onclick = function() {
+            this.classList.toggle("completed")
+        }
+    }
+    document.querySelector("#newtask input").value = "";
+ }
 }
+
